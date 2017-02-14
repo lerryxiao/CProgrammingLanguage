58603ffab71817013917a2b2895d67a30a773c8a
肖乐源 <shillyxiao36@gmail.com>
2017-02-14 22:45:26 +0800

    add chapter 1 1-1 and 1-2

---
+++ ch1/ex1-1.c
@@ -0,0 +1,13 @@
+/**
+ * 练习 1-1 在自己的系统中运行 "hello world" 程序。再有意去掉程序中的部分内容，看看会得到什么出错信息。
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, ");
+    printf("world");
+    printf("\n");
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+//
+// Created by 肖乐源 on 14/02/2017.
+//
+#include <stdio.h>
+
+int main()
+{
+   printf("C Programming Language is very good");
+}
